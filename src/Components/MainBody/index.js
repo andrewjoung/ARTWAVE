53e56e27d6e314313559dba435d05c27f6625efc
Andrew Joung <andrew.joung@gmail.com>
2019-08-05 10:15:46 -0700

    add pseudocode for database calls

--- src/Components/MainBody/index.js
+++ src/Components/MainBody/index.js
@@ -18,6 +18,28 @@ class MainBody extends Component {
         //create API route for database
         //get lists 
         //get recommendations
+
+        //TODO: 
+        //Use JSON dummy data => 
+        // if(this.state.category === cinema) {
+        //      newArray = resultOfAjaxListRequest.filter(where type = cinema)
+        //} else if (cateogry === literature) {
+        //      newARray = resultOfAjaxListRequest.filter(where type = literature)
+        //} else {
+        //      newARray= resultOfAjaxListRequest.filter(where type = music)
+        //}
+
+        /*
+            topPinned = newARray.filter(where stared = true);
+        
+            => some way to only display 4 pinned cards 
+                => displaying card with art 
+        
+        
+        */
+
+        //TODO: 
+        //repeat process above for recommended
     }
 
     render() {

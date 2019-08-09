39b11549847ab15013b8b02d7ae8aa92b35897b8
Dalinar Kholin <devinpoore@users.noreply.github.com>
2019-08-09 11:18:14 -0700

    Updates

---
+++ precache-manifest.dd6ea160bcb24e9fa6c5e00fc36e41d5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bacf04221de06b1d24d679d8a38ae5a",
+    "url": "/andrewjoung/ARTWAVE/index.html"
+  },
+  {
+    "revision": "139304a566c1272f747c",
+    "url": "/andrewjoung/ARTWAVE/static/css/main.e397f39b.chunk.css"
+  },
+  {
+    "revision": "8d821e9727d62ff0ca57",
+    "url": "/andrewjoung/ARTWAVE/static/js/2.f2f45d4b.chunk.js"
+  },
+  {
+    "revision": "139304a566c1272f747c",
+    "url": "/andrewjoung/ARTWAVE/static/js/main.72fa7ab5.chunk.js"
+  },
+  {
+    "revision": "ca804e9076766701aaaa",
+    "url": "/andrewjoung/ARTWAVE/static/js/runtime~main.98b13696.js"
+  },
+  {
+    "revision": "3cf3fdf1b98cb827813ab37dd49ea5fb",
+    "url": "/andrewjoung/ARTWAVE/static/media/testing2.3cf3fdf1.jpg"
+  },
+  {
+    "revision": "dad93726fb565ec4bd44174fe8f5a022",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture1.dad93726.jpg"
+  },
+  {
+    "revision": "3ba147f17cccd2fc9c524f8cf564d6f5",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture11.3ba147f1.jpg"
+  },
+  {
+    "revision": "d74d18204ae1b714437feeafbac60fe5",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture12.d74d1820.jpg"
+  },
+  {
+    "revision": "d1f89092f9e9d1060fde43dbfbea9780",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture2.d1f89092.jpg"
+  },
+  {
+    "revision": "005e18c9218786e759a85ff40c72790c",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture3.005e18c9.jpg"
+  },
+  {
+    "revision": "a62d113d36f60d915464a84dbf4489a4",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture4.a62d113d.jpg"
+  },
+  {
+    "revision": "d9589123f92eb9e7a816206368de7b82",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture5.d9589123.jpg"
+  },
+  {
+    "revision": "574f6675473597f8ad51f767052cb87e",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture6.574f6675.jpg"
+  },
+  {
+    "revision": "09463c99c8bf9467111e94303ff22308",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture8.09463c99.jpg"
+  },
+  {
+    "revision": "9db85107a950137ef2e4ae00745ca4be",
+    "url": "/andrewjoung/ARTWAVE/static/media/texture9.9db85107.jpg"
+  }
+]);
